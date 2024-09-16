7ee00bc01631985e5a66b68dc2649c59e86b0c71
Carvalho Allan <allan.carvalho@edu.esiee.fr>
2024-09-16 11:55:51 +0000

    Fonction qui teste si un mot est un palindrome

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 #### Fonction secondaire
-
+"""
+author : allan.carvalho@edu.esiee.fr
+"""
 
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """"
+    prend en argument une chaîne de caractère et regarde si
+    ce dernier est un palindrome 
+    """
+    if p[::-1] == p :
+        return True
     return False
 
 #### Fonction principale
 
-
 def main():
-
+    """
+    Teste quelques mots pour voir si 
+    la fonction marche
+    """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
